f1ee6eeb9086c8eb69bc3f58437dfe3aab13a6dc
Saw3rtt <sawertpra@gmail.com>
2023-06-06 14:45:20 +0300

    Задание 1-4

---
+++ src/Arrays.java
@@ -0,0 +1,51 @@
+public class Arrays {
+    public static void main(String[] args) {
+        task1();
+        ;
+
+    }
+
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+
+    public static void task1() {
+        int[] arr = generateRandomArray();
+        int total = 0;
+        for (int i = 0; i < arr.length; i++) {
+            total += arr[i];
+        }
+        System.out.println("Сумма трат за месяц составила " + total + " рублей");
+        //task2
+        int max = arr[0];
+        int min = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (min > arr[i]) {
+                min = arr[i];
+            }
+            if (max < arr[i]) {
+                max = arr[i];
+            }
+        }
+        System.out.println("Минимальная сумма трат за день составила " + min + " рублей");
+        System.out.println("Максимальная сумма трат за день составила " + max + " рублей");
+        //task3
+        double days = 30;
+        double total1 = 0;
+        for (int i = 0; i < arr.length; i++) {
+            total1 += arr[i];
+        }
+        System.out.printf("Средняя сумма трат за месяц составила %.2f рублей", total1 / days);
+        //task4
+        System.out.println();
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
+            System.out.print(reverseFullName[i]);
+        }
+    }
+}
